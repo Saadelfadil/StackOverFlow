1a3a71c1b2d2b88380bb9d575df13b6b978e6082
saadelfadil <saadelfadil18@gmail.com>
2024-01-16 19:47:44 +0100

    FIX ERROR

--- .history/components/shared/LeftNavbar_20240101194500.tsx
+++ .history/components/shared/LeftNavbar_20240101194500.tsx
@@ -14,7 +14,7 @@ const LeftNavbar = () => {
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
-            (pathname.includes(item.route) && item.route.length > 1) ||
+            (pathname.includes(item.route) && item.route.lenght > 1) ||
             pathname === item.route;
           return (
             <Link
